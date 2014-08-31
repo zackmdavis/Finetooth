240e932af032e875a46a0efeed1d929cf80117f6
Zack M. Davis <code@zackmdavis.net>
2014-08-30 23:30:09 -0700

    interpolate and populate color stops

--- core/colorize.py
+++ core/colorize.py
@@ -3,12 +3,35 @@ def diffract(hex_encoding):
 
 def undiffract(rgb):
     return "".join(hex(int(band))[2:].zfill(2) for band in rgb)
-    
+
 def interpolate(rgb1, rgb2, weighting):
-    return list(map(lambda c1, c2: c1 + weighting *(c2 - c1), rgb1, rgb2))
+    return list(map(lambda c1, c2: c1 + weighting*(c2 - c1), rgb1, rgb2))
 
 def interpolate_stop(color_stops, x):
     stops = sorted(color_stops.keys())
     closest_above = min(stop for stop in stops if (stop - x) > 0)
     closest_below = max(stop for stop in stops if (stop - x) < 0)
-    # TODO: finish
+    diffracted_above, diffracted_below = [
+        diffract(color_stops[s]) for s in (closest_above, closest_below)
+    ]
+    weighting = (x - closest_below) / (closest_above - closest_below)
+    return undiffract(
+        interpolate(diffracted_below, diffracted_above, weighting)
+    )
+
+def populate_stops(color_stops):
+    full_stops = color_stops.copy()
+    stops = color_stops.keys()
+    heroism = max(stops)
+    villainy = min(stops)
+    for moral_quality in range(villainy, heroism + 1):
+        if moral_quality not in color_stops:
+            full_stops[moral_quality] = interpolate_stop(
+                color_stops, moral_quality
+            )
+    return full_stops
+
+def style_block(value, color):
+    return "\n".join(["[data-value=\"{}\"] {{".format(value),
+                      "    color: #{};".format(color),
+                      "}\n"])
