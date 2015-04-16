e3fdc2e772eb590be5588aefbfcbb78e15cf0fb4
Zack M. Davis <code@zackmdavis.net>
2015-04-15 18:47:17 -0700

    make wsgi.py respect .development mechanism (to avoid dependency)

--- wsgi.py
+++ wsgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+
+if os.path.exists(".development"):
+    application = get_wsgi_application()
+else:
+    from dj_static import Cling
+    application = Cling(get_wsgi_application())
